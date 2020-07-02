d9beafa8f2c4bfc9f2e96e9e1308697c3b304f32
T1geress <tavitragus@gmail.com>
2020-07-02 11:16:37 -0500

    bound the buttons to the events 7/2

--- rock-paper-scissors/rockpaperscissors.py
+++ rock-paper-scissors/rockpaperscissors.py
@@ -1,5 +1,5 @@
 import random
-import tkinter
+import tkinter as tk
 
 #NOTE: A function that determines whether the user wins or not
 #      Passes the user's choice (based on what button they click)to the parameter
@@ -16,14 +16,15 @@ def get_winner(call):
         computerchoice = "rock"
     if randint == 2:
         computerchoice = "paper"
-    if randint == 4:
+    if randint == 3:
         computerchoice = "scissors"
 
-    # 3. Determine the winner based on what the user chose and what the computer chose 
+    # 3. Determine the winner based on what the user chose and what the computer chose
     #   Rock beats Scissors
     #   Paper beats Rock
     #   Scissors beats Paper
     #   It's a tie if the computer and user chose the same object
+
 
     # If the user wins, increase win by 1
     # Use the output label to write what the computer did and what the result was (win, loss, tie)
@@ -37,18 +38,46 @@ def pass_r():
 def pass_p():
     get_winner("paper")
 
-window = tkinter.Tk()
+window = tk.Tk()
 
 #Variable to count the number of wins the user gets
 win = 0
 
 
 #START CODING HERE
-
 # 1. Create 3 buttons for each option (rock, paper, scissors)
-
+rock = tk.Button(
+    text = 'Rock',
+    width = 5,
+    height = 2
+)
+paper = tk.Button(
+    text = 'Paper',
+    width = 5,
+    height = 2
+)
+scissors = tk.Button(
+    text = 'Scissors',
+    width = 5,
+    height = 2
+)
 # 2. Create 2 labels for the result and the number of wins
 
+winum = ('You have won ' + str(win) + ' times.')
 # 3. Arrange the buttons and labels using grid
+for i in range(3):
+    window.columnconfigure(i, weight=1, minsize=75)
+    window.rowconfigure(i, weight=1, minsize=50)
+
+    for j in range(0, 3):
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j, padx=5, pady=5)
+
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack(padx=5, pady=5)
 
 window.mainloop()
